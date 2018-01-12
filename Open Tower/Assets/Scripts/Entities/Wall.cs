64f32c36fb73b41bfb8fdd732ccb4e61f41d01af
Andrew X <me@andrewxue.com>
2018-01-12 13:37:32 -0500

    added walls

---
+++ Open Tower/Assets/Scripts/Entities/Wall.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wall : Entity {
+
+    protected override void DoAction(Player player) {
+        // Do nothing
+    }
+
+    protected override bool IsActionPossible(Player player) {
+        return true;
+    }
+}
