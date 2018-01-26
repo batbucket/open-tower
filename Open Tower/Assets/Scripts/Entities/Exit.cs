d5de5a38f2e43c11a45bf1c0eb00fc6ef674c8d5
Andrew X <me@andrewxue.com>
2018-01-26 01:28:29 -0500

    scene now transitions correctly

---
+++ Open Tower/Assets/Scripts/Entities/Exit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Exit : Entity {
+
+    [SerializeField]
+    private string destinationScene;
+
+    protected override void DoAction(Player player) {
+        SceneManager.LoadScene(destinationScene);
+    }
+
+    protected override bool IsActionPossible(Player player) {
+        return true;
+    }
+}
