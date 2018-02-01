f1197c49580c34472afef860efc5d219c19f91bc
Andrew X <me@andrewxue.com>
2018-02-01 04:39:17 -0500

    enemy stats can now be made visible by holding space

---
+++ Open Tower/Assets/Scripts/Managers/EnemyStatsDisplay.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyStatsDisplay : MonoBehaviour {
+
+    [SerializeField]
+    private Stats enemy;
+
+    [SerializeField]
+    private Text life;
+
+    [SerializeField]
+    private Text power;
+
+    [SerializeField]
+    private Text defense;
+
+    [SerializeField]
+    private Text experience;
+
+    [SerializeField]
+    private GameObject wrapper;
+
+    private void Start() {
+        life.text = enemy.Life.ToString();
+        power.text = enemy.Power.ToString();
+        defense.text = enemy.Defense.ToString();
+        experience.text = enemy.Experience.ToString();
+
+        if (enemy.Experience == 0) {
+            Destroy(experience.transform.parent.gameObject);
+        }
+    }
+
+    private void Update() {
+        wrapper.SetActive(Input.GetKey(KeyCode.Space));
+    }
+}
