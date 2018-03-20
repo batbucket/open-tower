86578bd46075388f48be84e2e62a2c3a20417d79
Andrew X <me@andrewxue.com>
2018-03-20 01:33:01 -0400

    sprite animator now goes faster

--- Open Tower/Assets/Scripts/Entities/Enemy/SpriteAnimator.cs
+++ Open Tower/Assets/Scripts/Entities/Enemy/SpriteAnimator.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteAnimator : MonoBehaviour {
-    private const float secondsPerFrame = 1f;
+    private const float secondsPerFrame = 0.5f;
 
     [SerializeField]
     private Sprite[] sprites;
@@ -11,8 +11,11 @@ public class SpriteAnimator : MonoBehaviour {
     private new SpriteRenderer renderer;
     private Coroutine routine;
 
+    private Transform rendererT;
+
     private void Start() {
         renderer = GetComponentInChildren<SpriteRenderer>(true);
+        rendererT = renderer.transform;
     }
 
     private void OnEnable() {
@@ -25,13 +28,27 @@ public class SpriteAnimator : MonoBehaviour {
         }
     }
 
+    private IEnumerator DoLazyAnimation() {
+        Start();
+        while (true) {
+            yield return Util.Lerp(1, t => {
+                rendererT.localPosition = Vector2.Lerp(new Vector2(0, -0.95f), new Vector2(0, 0.95f), t);
+                rendererT.localScale = Vector2.Lerp(new Vector2(1, 0.95f), new Vector2(1, 1.05f), t);
+            });
+            yield return Util.Lerp(1, t => {
+                rendererT.localPosition = Vector2.Lerp(new Vector2(0, 0.95f), new Vector2(0, -0.95f), t);
+                rendererT.localScale = Vector2.Lerp(new Vector2(1, 1.05f), new Vector2(1, 0.95f), t);
+            });
+        }
+    }
+
     private IEnumerator DoAnimation() {
         while (true) {
             foreach (Sprite sprite in sprites) {
                 yield return new WaitForSeconds(secondsPerFrame);
                 renderer.sprite = sprite;
-                yield return null;
             }
+            yield return null;
         }
     }
 }
