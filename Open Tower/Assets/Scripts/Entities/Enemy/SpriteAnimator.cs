f1e67788c3a252f3ad1582058721e8211e972106
Andrew X <me@andrewxue.com>
2018-03-20 00:47:27 -0400

    made sprite animator script. untested

--- Open Tower/Assets/Scripts/Entities/Enemy/SpriteAnimator.cs
+++ Open Tower/Assets/Scripts/Entities/Enemy/SpriteAnimator.cs
@@ -3,4 +3,35 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class SpriteAnimator : MonoBehaviour {
+    private const float secondsPerFrame = 1f;
+
+    [SerializeField]
+    private Sprite[] sprites;
+
+    private new SpriteRenderer renderer;
+    private Coroutine routine;
+
+    private void Start() {
+        renderer = GetComponentInChildren<SpriteRenderer>(true);
+    }
+
+    private void OnEnable() {
+        routine = StartCoroutine(DoAnimation());
+    }
+
+    private void OnDisable() {
+        if (routine != null) {
+            StopCoroutine(routine);
+        }
+    }
+
+    private IEnumerator DoAnimation() {
+        while (true) {
+            foreach (Sprite sprite in sprites) {
+                yield return new WaitForSeconds(secondsPerFrame);
+                renderer.sprite = sprite;
+                yield return null;
+            }
+        }
+    }
 }
